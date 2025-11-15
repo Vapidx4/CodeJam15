1e9265a46ef1747341eef9c3dddb18ca527046b8
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2025-11-15 02:59:41 -0500

    ppoopk

--- my-expo-app/app/test1.tsx
+++ my-expo-app/app/test1.tsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native";
- 
+import { useEffect, useState, useRef } from "react";
+import { Text, View, AppState } from "react-native";
+
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+    const [activeCount, setActiveCount] = useState(0);
+    const actualCount = Math.floor((activeCount + 1) / 2);
+
+    const isMounted = useRef(false);
+
+    useEffect(() => {
+        isMounted.current = true;
+        
+        const subscription = AppState.addEventListener("change", (nextAppState) => {
+            if (nextAppState === "active" || nextAppState === "background") {
+                setActiveCount((count) => count + 1);
+            }
+        });
+
+        return () => {
+            isMounted.current = false;
+            subscription.remove();
+        };
+    }, []);
+
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
+            <Text className="mt-4 text-lg">
+                AppState was active {actualCount} times
+            </Text>
+        </View>
+    );
 }
